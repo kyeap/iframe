9945e6f634785256119dfdedb50f52918733f00f
ken <kenjoo.dev@gmail.com>
2023-03-13 15:58:04 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+
+const Container = styled.div`
+  min-height: 100vh;
+  display: grid;
+  grid-template-rows: 4rem 1fr auto;
+  grid-template-columns: 258px 1fr;
+  grid-template-areas:
+    "header header"
+    "menu main"
+    "footer footer";
+`;
+
+const ContentContainer = styled.main`
+  grid-area: main;
+  padding: 2rem 1rem;
+  overflow: hidden;
+
+  @media screen and (min-width: 768px) {
+    padding: 2rem 3.5rem;
+  }
+`;
+
+const Header = styled.div`
+  grid-area: header;
+`;
+
+const Menu = styled.div`
+  grid-area: menu;
+  height: 100%;
+  background: grey;
+`;
+
+const Footer = styled.footer`
+  grid-area: footer;
+  background: blue;
+  height: 10rem;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Header>This is within the B2B portal</Header>
+        <Menu>
+          Menu
+          <ul>
+            <li>Dashboard</li>
+            <li>Wallbox</li>
+            <li>Create order</li>
+            <li>Validate order</li>
+            <li>Overview</li>
+            <li>Link Management</li>
+          </ul>
+        </Menu>
+        <iframe
+          src="https://gronnkontakt-pp.driivz.com:8443/dashboard"
+          width={window.innerWidth - 258}
+          height={window.innerHeight}
+        ></iframe>
+        <Footer>Footer</Footer>
+      </Container>
     </div>
   );
 }
